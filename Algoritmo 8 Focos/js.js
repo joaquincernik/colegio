86ff2a6bed362a7e0dc5b063fc00af47b5a1b06a
joaquincernik <joaquincernik@gmail.com>
2020-06-07 17:05:00 -0300

    Octavo

---
+++ Algoritmo 8 Focos/js.js
@@ -0,0 +1,18 @@
+var rojo = 3;
+var blanco = 0;
+var verde = 2;
+
+
+ 
+ 
+function myFunction(){
+  
+    document.getElementById("demo").innerHTML= "Hay "+ rojo + " luces rojas" + "<br> Hay " + blanco + " luces blancas " + "<br> Hay " + verde + " luces verdes";
+ 
+}
+
+    
+                 
+
+                
+    

92c4968d0ba3853180ee17f6c35c0b44ac6cb9b5
joaquincernik <joaquincernik@gmail.com>
2020-06-07 16:59:42 -0300

    ffff

---
+++ Algoritmo 1 colectivo/js.js
@@ -0,0 +1,11 @@
+
+ var costofinal;
+ var kilometros_correr= prompt("Cuantos kilometros vamos a recorrer?");
+ var kilometros_costo= prompt("Cual es el costo por kilometro?");
+function myFunction(){
+    
+   var costofinal= kilometros_correr*kilometros_costo ;
+   document.write(costofinal);
+}
+ 
+ 

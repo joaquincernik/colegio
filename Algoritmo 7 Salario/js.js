a5d4858caa3ec29da1972b98f1e5b1a5d069b931
joaquincernik <joaquincernik@gmail.com>
2020-06-07 17:03:59 -0300

    Septimo

---
+++ Algoritmo 7 Salario/js.js
@@ -0,0 +1,40 @@
+ 
+ var sueldo = 1500;
+ var a = 1500*10/100;
+ var aumento1= sueldo + a ;
+
+ var a2 = aumento1*10/100;
+ var aumento2= aumento1 + a2 ;
+
+ var a3 = aumento2*10/100;
+ var aumento3= aumento2 + a3 ;
+
+ var a4 = aumento3*10/100;
+ var aumento4= aumento3 + a4 ;
+
+ var a5 = aumento4*10/100;
+ var aumento5= aumento4 + a5 ;
+
+ var a6 = aumento5*10/100;
+ var aumento6= aumento5 + a6 ;
+
+ var x = parseInt(aumento1);
+ var y = parseInt(aumento2);
+ var z = parseInt(aumento3);
+ var d = parseInt(aumento4);
+ var e = parseInt(aumento5);
+ var f = parseInt(aumento6);
+
+
+ 
+ 
+function myFunction(){
+    document.write("Año 1 : "+ x + "<br> Año 2: " + y + "<br> Año 3: " + z + "<br> Año 4: " + d + "<br> Año 5: " + e + "<br> Año 6: " + f )
+   
+}
+
+    
+                 
+
+                
+    
